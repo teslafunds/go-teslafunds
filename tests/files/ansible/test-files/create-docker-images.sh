d8cd3d01be35edf0abcfb0da529d65d674120dfc
teslafunds <43968576+teslafunds@users.noreply.github.com>
2021-04-28 10:20:50 +0200

    Delete tests directory

--- tests/files/ansible/test-files/create-docker-images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-# creates the necessary docker images to run testrunner.sh locally
-
-docker build --tag="teslafunds/cppjit-testrunner" docker-cppjit
-docker build --tag="teslafunds/python-testrunner" docker-python
-docker build --tag="teslafunds/go-testrunner" docker-go
